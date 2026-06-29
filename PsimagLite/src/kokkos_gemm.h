a61c879981df388074ac4ad45482d8cc5d991aaa
Daniel Arndt <arndtd@ornl.gov>
2026-06-29 09:53:16 -0400

    Revert "Back to CPU still failing"
    This reverts commit 297ff72b6cee646a6e8265e5917367799945db91.

--- PsimagLite/src/kokkos_gemm.h
+++ PsimagLite/src/kokkos_gemm.h
@@ -55,8 +55,8 @@ inline void kokkos_gemm(char transa,
     // Determine Kokkos scalar type
         using KokkosScalar = KokkosType<Scalar>::type;
 
-Kokkos::Serial exec;
-Kokkos::HostSpace mem;
+Kokkos::DefaultExecutionSpace exec;
+Kokkos::DefaultExecutionSpace::memory_space mem;
 
         Kokkos::View<KokkosScalar**, Kokkos::LayoutLeft, Kokkos::HostSpace> Aview_op("Aview", M, K);
         if (ta == 'N') {
@@ -114,7 +114,7 @@ Kokkos::HostSpace mem;
         }
         auto Bview_op_device = Kokkos::create_mirror_view_and_copy(Kokkos::view_alloc(exec, mem), Bview_op);
 
-        Kokkos::View<KokkosScalar**, decltype(mem)> Cview_device("Cview", M, N);
+        Kokkos::View<KokkosScalar**> Cview_device("Cview", M, N);
 
         const char transNN[2] = {'N', '\0'};
         KokkosBlas::gemm(exec, transNN, transNN, alpha, Aview_op_device, Bview_op_device, beta, Cview_device);
